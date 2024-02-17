4e544306d00a5071a2138f993c2856c794d1db40
Radha kumari <145616288+radhaR15@users.noreply.github.com>
2024-02-17 22:24:32 +0530

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,7 @@
+echo "# JAVASCRIPT" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/radhaR15/JAVASCRIPT.git
+git push -u origin main
